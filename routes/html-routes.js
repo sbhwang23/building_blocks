57fff5e7893c58e993e3f8b87e24bef85a2ef997
Ameco <hamecow@gmail.com>
2020-09-27 16:34:27 -0700

    Add path to login.hbs in htl-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,25 +1,28 @@
 const path = require("path");
+//const express = require('express')
+//const app = express()
+
 
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  app.get("/", (req, res) => {
-    
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../views/login.hbs"));
+        //if (req.user) {
+        //    res.redirect("/members");
+        //}
+        //res.sendFile(path.join(__dirname, "../public/js/signup.html"));
+    });
 
-  app.get("/login", (req, res) => {
-    
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+    app.get("/login", (req, res) => {
 
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
+        if (req.user) {
+            res.redirect("/members");
+        }
+        res.sendFile(path.join(__dirname, "../public/js/login.html"));
+    });
+
+    app.get("/members", isAuthenticated, (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/js/members.html"));
+    });
 };
