c4676b0ccf91b38141a47ce653b1313c102ba3c3
grimmgr <ggrimm33@gmail.com>
2020-09-30 20:39:20 -0700

    homepage redirect buttons

--- public/js/add-bucketlist.js
+++ public/js/add-bucketlist.js
@@ -3,22 +3,20 @@ $(function() {
     $(".create-form").on("submit", (event) => {
         event.preventDefault();
 
-        console.log("button clicked");
+        const newBucketListItem = {
+            title: $("#title").val().trim(),
+            description: $("#description").val().trim(),
+            category: $("input[name='category']:checked").val().trim(),
+            collaborators: $("input[name='collaborators']:checked").val().trim(),
+            location: $("#location").val().trim()
+        };
 
-        // const newBucketListItem = {
-        //     title: $("#title").val().trim(),
-        //     description: $("#description").val().trim(),
-        //     category: $("input[name='category']:checked").val().trim(),
-        //     collaborators: $("input[name='collaborators']:checked").val().trim(),
-        //     location: $("#location").val().trim()
-        // };
-
-        // $.ajax("/api/bucket-list", {
-        //     type: 'POST',
-        //     data: newBucketListItem
-        // }).then(() => {
-        //     console.log("bucket list item added");
-        // })
+        $.ajax("/api/bucket-list", {
+            type: 'POST',
+            data: newBucketListItem
+        }).then(() => {
+            console.log("bucket list item added");
+        })
 
     });
 
--- public/js/members.js
+++ public/js/members.js
@@ -3,4 +3,24 @@ $(document).ready(() => {
     $.get("/api/user_data").then(data => {
         $(".member-name").text(data.email);
     });
+
+    $(".image-square-1").on("click", (event) => {
+        event.preventDefault();
+        window.location.replace("/mybucketlist");
+    });
+
+    $(".image-square-2").on("click", (event) => {
+        event.preventDefault();
+        window.location.replace("/newactivity");
+    });
+
+    $(".image-square-3").on("click", (event) => {
+        event.preventDefault();
+        window.location.replace("/search");
+    });
+
+    $(".image-square-4").on("click", (event) => {
+        event.preventDefault();
+        window.location.replace("/map");
+    });
 });
