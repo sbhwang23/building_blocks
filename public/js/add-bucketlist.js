a6aa5ff610d172838dfb8746f633d6b37e38bc8c
grimmgr <ggrimm33@gmail.com>
2020-09-30 19:57:13 -0700

    fix new-activity.hbs

--- public/js/add-bucketlist.js
+++ public/js/add-bucketlist.js
@@ -1,22 +1,25 @@
-$(document).ready(() => {
+$(function() {
 
-    $("#submit").on("click", event => {
+    $(".create-form").on("submit", (event) => {
         event.preventDefault();
-        const newBucketListItem = {
-            title: $("#title").val().trim(),
-            description: $("#description").val().trim(),
-            category: $("input[name='category']:checked").val().trim(),
-            collaborators: $("input[name='collaborators']:checked").val().trim(),
-            location: $("#location").val().trim()
-        };
 
-        $.ajax("/api/bucket-list", {
-            type: 'POST',
-            data: newBucketListItem
-        }).then(() => {
-            location.reload();
-        })
+        console.log("button clicked");
 
-    })
+        // const newBucketListItem = {
+        //     title: $("#title").val().trim(),
+        //     description: $("#description").val().trim(),
+        //     category: $("input[name='category']:checked").val().trim(),
+        //     collaborators: $("input[name='collaborators']:checked").val().trim(),
+        //     location: $("#location").val().trim()
+        // };
+
+        // $.ajax("/api/bucket-list", {
+        //     type: 'POST',
+        //     data: newBucketListItem
+        // }).then(() => {
+        //     console.log("bucket list item added");
+        // })
+
+    });
 
 });
