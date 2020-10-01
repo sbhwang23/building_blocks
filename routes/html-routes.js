41a20b6126a2b6252f88d0711f564f94a5f9c5db
grimmgr <ggrimm33@gmail.com>
2020-10-01 10:29:57 -0700

    hadlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,26 +5,39 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function (app) {
     //LANDING HOME PAGE where user can login OR JOIN
     app.get("/", (req, res) => {
-        res.render("loginpage", { layout: "login" });
+        res.render("loginpage", { 
+            style: "login.css"
+        });
     });
     //SIGN UP PAGE
     app.get("/join", (req, res) => {
-        res.render("signuppage", { layout: "signup" });
+        res.render("signuppage", {
+            style: "signup.css"
+        });
     });
     app.get("/member", //isAuthenticated,
      (req, res) => {
-        res.render("member", { layout: "main" });
+        res.render("member", {
+            style: "style.css" });
     });
     app.get("/map", (req, res) => {
-        res.render("maps", { layout: "activities" });
+        res.render("maps", {
+            style: "map.css"
+        });
     });
     app.get("/mybucketlist", (req, res) => {
-        res.render("mybucketlist", { layout: "activities" });
+        res.render("mybucketlist", {
+            style: "style.css"
+        });
     });
     app.get("/newactivity", (req, res) => {
-        res.render("new-activity", { layout: "activities" });
+        res.render("new-activity", { 
+            style: "style.css"
+        });
     });
     app.get("/search", (req, res) => {
-        res.render("search", { layout: "activities" });
+        res.render("search", {
+            style: "search.css"
+        });
     });
 };
