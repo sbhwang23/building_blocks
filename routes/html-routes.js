60569879b6654c7f0d88621ebba5708d42a90c2b
Sarah <soobin.sarah.hwang@gmail.com>
2020-10-01 20:32:21 -0700

    is Authenticated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,13 +1,17 @@
-const path = require("path");
-
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
     //LANDING HOME PAGE where user can login OR JOIN
     app.get("/", (req, res) => {
-        res.render("loginpage", {
-            style: "login.css"
-        });
+        if (req.isAuthenticated()) {
+            res.render("member", {
+                style: "member.css"
+            });
+        } else {
+            res.render("loginpage", {
+                style: "login.css"
+            });
+        }
     });
     //SIGN UP PAGE
     app.get("/join", (req, res) => {
@@ -15,30 +19,34 @@ module.exports = function(app) {
             style: "signup.css"
         });
     });
-    app.get("/member", //isAuthenticated,
+    app.get("/member", isAuthenticated,
         (req, res) => {
             res.render("member", {
                 style: "member.css"
             });
         });
-    app.get("/map", (req, res) => {
-        res.render("maps", {
-            style: "map.css"
+    app.get("/map", isAuthenticated,
+        (req, res) => {
+            res.render("maps", {
+                style: "map.css"
+            });
         });
-    });
-    app.get("/mybucketlist", (req, res) => {
-        res.render("mybucketlist", {
-            style: "style.css"
+    app.get("/mybucketlist", isAuthenticated,
+        (req, res) => {
+            res.render("mybucketlist", {
+                style: "style.css"
+            });
         });
-    });
-    app.get("/newactivity", (req, res) => {
-        res.render("new-activity", {
-            style: "style.css"
+    app.get("/newactivity", isAuthenticated,
+        (req, res) => {
+            res.render("new-activity", {
+                style: "style.css"
+            });
         });
-    });
-    app.get("/search", (req, res) => {
-        res.render("search", {
-            style: "search.css"
+    app.get("/search", isAuthenticated,
+        (req, res) => {
+            res.render("search", {
+                style: "search.css"
+            });
         });
-    });
 };
