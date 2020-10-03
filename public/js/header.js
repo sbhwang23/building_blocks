d770f77d5a4bc5ce7d296ab64a016d93fd17c56d
Sarah <soobin.sarah.hwang@gmail.com>
2020-10-03 11:53:27 -0700

    link for my list

---
+++ public/js/header.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+$("#list").on("click", (event) => {
+    event.preventDefault();
+    $.get("/api/user_data").then(data => {
+        const userId = data.id;
+        window.location.replace(`/mybucketlist/${userId}`);
+    });
+});
+});
